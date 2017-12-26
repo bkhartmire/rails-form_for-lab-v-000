b2cab9b5c133ee50142d365dcba377cd4f2604a8
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-26 16:20:23 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.create()
+    @student = Student.create(student_params(:first_name, :last_name))
     redirect_to student_path(@student)
   end
 
